ce024b49fbb522835c6f2bd8fa7315324fbb0ed7
kingdon <kinpauln@126.com>
2014-04-03 17:01:48 +0800

    20140403

---
+++ FreightForwarder.Domain/Entities/UpgradePackage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace FreightForwarder.Domain.Entities
+{
+    [Table("UpgradePackage")]
+    [Serializable]
+    [DataContract]
+    public class UpgradePackage
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string FileVersion { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        public byte[] FileBytes { get; set; }
+
+        [DataMember]
+        public DateTime PostTime { get; set; }
+    }
+}
