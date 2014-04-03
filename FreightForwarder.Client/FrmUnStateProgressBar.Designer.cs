ca46dc23fe84d9d20e9573f1841c17af80424e2a
kingdon <kinpauln@126.com>
2014-04-03 14:20:42 +0800

    20140403-1

--- FreightForwarder.Client/FrmUnStateProgressBar.Designer.cs
+++ FreightForwarder.Client/FrmUnStateProgressBar.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUnStateProgressBar));
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // progressBar1
@@ -61,13 +64,29 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTimer.Location = new System.Drawing.Point(206, 20);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(17, 16);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // FrmUnStateProgressBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 112);
+            this.ClientSize = new System.Drawing.Size(454, 108);
             this.ControlBox = false;
             this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblTimer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -75,6 +94,7 @@
             this.Name = "FrmUnStateProgressBar";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmUnStateProgressBar_FormClosed);
             this.Load += new System.EventHandler(this.FrmUnStateProgressBar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -86,5 +106,7 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- FreightForwarder.Client/FrmUnStateProgressBar.cs
+++ FreightForwarder.Client/FrmUnStateProgressBar.cs
@@ -14,6 +14,9 @@ namespace FreightForwarder.UI.Winform
     {
         public string DisplayInfo = "正在执行，请耐心等待。。。。";
         public bool ShowCancel = true;
+        private int _mitute;
+        private int _second;
+        private int _msencond;
 
         public FrmUnStateProgressBar()
         {
@@ -25,11 +28,38 @@ namespace FreightForwarder.UI.Winform
             label1.Text = DisplayInfo;
 
             btnCancel.Visible = ShowCancel;
+
+            _mitute = 0;
+            _second = 0;
+            _msencond = 0;
+
+            timer1.Start();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        private void FrmUnStateProgressBar_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _msencond += 10;
+            if (_msencond == 1000)
+            {
+                _second += 1;
+                _msencond = 0;
+            }
+            if (_second == 60)
+            {
+                _mitute += 1;
+                _second = 0;
+            }
+            lblTimer.Text = (_mitute > 0 ? _mitute + "分:" : string.Empty) + (_second + "秒:") + (_msencond > 0 ? _msencond.ToString() : string.Empty);
         }
     }
 }
